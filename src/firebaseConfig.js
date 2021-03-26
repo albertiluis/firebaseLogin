31a6a88122b9366ca077bd333a73e2220262ae51
Luis Henrique Alberti <luishenrialberti@gmail.com>
2021-03-26 15:07:35 -0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,9 @@
 export default {
-    apiKey: "AIzaSyBg-zsRULqwvaZOKJo75RVKUnoYnkEMT-Y",
-    authDomain: "validacaoreactjs.firebaseapp.com",
-    projectId: "validacaoreactjs",
-    storageBucket: "validacaoreactjs.appspot.com",
-    messagingSenderId: "868876991822",
-    appId: "1:868876991822:web:dabf4b428a392483fb321f"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   
